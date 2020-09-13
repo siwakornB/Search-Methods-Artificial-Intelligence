47af3d7f3333c1d991c2425ff59a51e761d262d1
kkeng <kkeng_patchara@outlook.com>
2020-09-13 12:45:05 +0700

    update time delay

--- word-search.py
+++ word-search.py
@@ -7,11 +7,8 @@ import datetime
 # import puzzel generetor
 from word_search_puzzle.utils import display_panel
 from word_search_puzzle.algorithms import create_panel
-<<<<<<< Updated upstream
-=======
 # for memory tracer
 import tracemalloc
->>>>>>> Stashed changes
 
 pygame.font.init()
 myfont = pygame.font.SysFont("comicsans", 40)
@@ -20,36 +17,6 @@ litfont = pygame.font.SysFont("comicsans", 30)
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
 
-<<<<<<< Updated upstream
-start_status = 0
-reset_status = 0
-InputBox_status = 0
-
-WIDTH,HEIGHT = 1366,768
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Test")
-
-blockSize = 40 #Set the size of the grid block
-table = [['C','O','N','N','E','C','T','I','O','N'],
-            ['F','C','E','L','L','P','H','O','N','E'],
-            ['A','R','Z','T','S','P','E','E','C','H'],
-            ['C','B','T','M','A','L','U','J','G','G'],
-            ['I','A','L','I','F','I','U','G','B','I'],
-            ['A','S','E','N','I','H','C','A','M','P'],
-            ['L','L','H','P','B','E','J','O','S','H'],
-            ['K','R','G','R','J','S','I','R','I','O'],
-            ['M','G','Z','D','E','E','P','S','M','N'],
-            ['D','I','N','T','E','R','N','E','T','E'] ]
-WORDS = ['AI','FACIAL','SPEECH','CONNECTION','INTERNET',
-            'IPHONE','SIRI','CELLPHONE',]               #'MACHINES','SPEED'
-words = WORDS.copy()
-visited = []
-
-class DFS():
-    def __init__(self):
-        self.run = True #initial with running state
-        self.rootx = 0 #just for keeping root
-=======
 WIDTH, HEIGHT = 1366, 768
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Words Search")
@@ -98,17 +65,10 @@ class DFS():  # depth first search
     def __init__(self):
         self.run = False  # initial with running state
         self.rootx = 0  # just for keeping root
->>>>>>> Stashed changes
         self.rooty = 0
         self.x = 0  # for keeping recent search
         self.y = 0
         self.string = ''
-<<<<<<< Updated upstream
-        self.dir = 0 #0-3 for directory
-        self.directory = ['NorthEast','East','SouthEast','South']
-        self.path = []  #in case of highlighting word
-    
-=======
         self.dir = 0  # 0-3 for directory
         self.directory = ['NorthEast', 'East', 'SouthEast', 'South']
         self.path = []  # in case of highlighting word
@@ -119,99 +79,12 @@ class DFS():  # depth first search
         self.words = WORDS.copy()
         self.visited = []
 
->>>>>>> Stashed changes
     def nextRoot(self):
         if (self.rooty < 9):
             self.rooty += 1
         elif (self.rootx < 9):
             self.rooty = 0
             self.rootx += 1
-<<<<<<< Updated upstream
-        else:
-            self.run = False
-            
-        #print('Root' + ':' + table[self.rootx][self.rooty])
-        self.redraw()
-
-    def search(self):
-        #print('x =',self.x,'y = ',self.y,'dir = ',self.dir)
-        if(self.x >= 0 and self.x <= 9):
-            if (self.y >= 0 and self.y <= 9):
-                #print(self.directory[self.dir] + ':' + table[self.x][self.y])
-                self.string = self.string + table[self.x][self.y]
-                #print(self.string)
-                self.redraw()
-                if self.string in words:
-                    print("------------------------------------------------------")
-                    self.path.append(self.string)
-                    words.remove(self.string)
-                    visited.append(self.string)
-                    print(visited)
-                    self.dir += 1 
-                    if(self.dir > 3):
-                        self.dir = 0
-                        self.nextRoot()
-                    self.x,self.y = self.rootx,self.rooty
-                    self.string = ''
-                    if(len(words) <= 0):
-                        self.run = False
-                if (self.dir == 0):
-                    #search(PosX-1, PosY+1, string, dir, position)
-                    self.x,self.y =  self.x-1,self.y+1
-                elif (self.dir == 1):
-                    #search(PosX, PosY+1, string, dir, position)
-                    self.y =  self.y+1
-                elif (self.dir == 2):
-                    #search(PosX+1, PosY, string, dir, position)
-                    self.x,self.y =  self.x+1,self.y+1
-                elif (self.dir == 3):
-                    #search(PosX + 1, PosY+1, string, dir, position)
-                    self.x =  self.x+1
-                
-            else:
-                self.dir += 1 
-                if(self.dir > 3):
-                    self.dir = 0
-                    self.nextRoot()
-                self.x,self.y = self.rootx,self.rooty
-                self.string = ''
-        else:
-            self.dir += 1
-            if(self.dir > 3):
-                    self.dir = 0
-                    self.nextRoot()
-            self.x,self.y = self.rootx,self.rooty
-            self.string = ''
-
-    def circle(self):
-        pygame.draw.circle(SCREEN, (0,0,255), (self.rooty*blockSize+blockSize//4+2,self.rootx*blockSize+blockSize//4+5), blockSize//4,2)
-        pygame.draw.circle(SCREEN, (0,255,0), (self.y*blockSize+blockSize//4+2,self.x*blockSize+blockSize//4+5), blockSize//4,2)
-  
-    def redraw(self):
-        SCREEN.fill(BLACK)
-        drawGrid()
-        # start_button = pygame.Rect(800, 200, 200, 100)
-        # reset_button = pygame.Rect(800, 600, 200, 100)
-        StartResetButton = pygame.Rect((WIDTH*2/7)+200, HEIGHT*4/5, 100, 50)
-        
-        # pygame.draw.rect(SCREEN, (255, 255, 0), start_button)
-        # pygame.draw.rect(SCREEN, (255,0,0), reset_button)
-        pygame.draw.rect(SCREEN, (255, 255, 0), StartResetButton)
-        self.circle()
-
-        pygame.display.update()
-
-def main():
-    global start_status
-    global reset_status
-    global words
-    global WORDS
-    global myfont
-    global InputBox_status
-    
-    
-    StartResetButton_status = 'start'  
-=======
 
         # print('Root' + ':' + table[self.rootx][self.rooty])
 
@@ -422,22 +295,14 @@ def main():
 
     StartResetButton_status = True
     InputBox_status = True
->>>>>>> Stashed changes
 
     FPS = 60
     CLOCK = pygame.time.Clock()
     timer = pygame.time.get_ticks()
     timer2 = pygame.time.get_ticks()
 
-<<<<<<< Updated upstream
-    #define button
-    # start_button = pygame.Rect(WIDTH*2/7, HEIGHT*4/5, 100, 50)
-    # reset_button = pygame.Rect((WIDTH*2/7)+200, HEIGHT*4/5, 100, 50)
-    StartResetButton = pygame.Rect((WIDTH*2/7)+200, HEIGHT*4/5, 100, 50)
-=======
     # define button
     StartResetButton = pygame.Rect((WIDTH * 2 / 7) + 200, HEIGHT * 4 / 5, 100, 50)
->>>>>>> Stashed changes
 
     # define input box
     input_box = pygame.Rect((WIDTH * 2 / 7) + 400, (HEIGHT * 4 / 5), 100, 50)
@@ -454,48 +319,6 @@ def main():
     textRect.center = ((WIDTH * 2 / 7) + 350, HEIGHT * 4 / 5 + 25)
 
     dfs = DFS()
-<<<<<<< Updated upstream
-
-    while True:
-        CLOCK.tick(FPS)
-        t = pygame.time.get_ticks() - timer
-        #print(t)
-        # if(dfs.run):
-        #     dfs.search()
-        #     timer = pygame.time.get_ticks()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:          #button click
-                if event.button == 1:                           #1 is the left mouse button, 2 is middle, 3 is right.
-                    if StartResetButton.collidepoint(event.pos):
-                        if StartResetButton_status == 'start':
-                            start_status = 1
-                            InputBox_status = 1
-                            print(StartResetButton_status)
-                            StartResetButton_status = 'reset'
-                        elif StartResetButton_status == 'reset':
-                            SCREEN.fill(BLACK)
-                            start_status = 0
-                            words = WORDS.copy()
-                            visited.clear()
-                            InputBox_status = 0
-                            StartResetButton_status = 'reset'
-                            main()
-                    # if start_button.collidepoint(event.pos):
-                    #     print("-------------------------------------------------------------------")
-                    #     start_status = 1
-                    #     InputBox_status = 1
-                    # if reset_button.collidepoint(event.pos):
-                    #     print("-------------------------------------------------------------------")
-                    #     SCREEN.fill(BLACK)
-                    #     start_status = 0
-                    #     words = WORDS.copy()
-                    #     visited.clear()
-                    #     InputBox_status = 0
-                    #     main()
-=======
     iddfs = IDDFS()
     GAME = True
 
@@ -571,8 +394,6 @@ def main():
                 if event.button == 1:  # 1 is the left mouse button, 2 is middle, 3 is right
                     if StartResetButton.collidepoint(event.pos):
                         delay = int(text)
-                        # print(type(delay))
-                        print(delay)
                         if StartResetButton_status:
                             print("------------------------START----------------------------")
                             dfs.run = True
@@ -585,53 +406,25 @@ def main():
                             iddfs.reset()
                             InputBox_status = True
                             StartResetButton_status = not StartResetButton_status
->>>>>>> Stashed changes
+                            text = '0'
                     if input_box.collidepoint(event.pos):
                         active = not active
                     else:
                         active = False
                     color = color_active if active else color_inactive
             elif event.type == pygame.KEYDOWN:
-                if InputBox_status == 0:
+                if InputBox_status:
                     if event.key == pygame.K_RETURN:
                         # print(text)
                         text = ''
                     if event.key == pygame.K_BACKSPACE:
                         text = text[:-1]
-<<<<<<< Updated upstream
-                        #print(text)
-                        InputBox_status = 0
-                    else:
-                        text += event.unicode
-                        #print(text)
-        SCREEN.fill(BLACK)
-        drawGrid()
-        if(start_status == 1):
-            if(dfs.run):
-                dfs.search()  
-                timer = pygame.time.get_ticks()
-        # pygame.draw.rect(SCREEN, (255, 255, 0), start_button)
-        # pygame.draw.rect(SCREEN, (255,0,0), reset_button)
-        pygame.draw.rect(SCREEN, (255, 255, 0), StartResetButton)
-        # print(text)
-        if InputBox_status == 0:
-            txt_surface = myfont.render(text, True, color)
-            width = max(200, txt_surface.get_width()+10)
-            input_box.w = width
-            SCREEN.blit(txt_surface, (input_box.x+5, input_box.y+5))
-            pygame.draw.rect(SCREEN, color, input_box, 2)
-        SCREEN.blit(text_delay, textRect) 
-        
-        
-        pygame.display.update()
-=======
                         # print(text)
                     elif ord(event.unicode) > 0 and ord(event.unicode) <= 1000:
                         text += event.unicode
 
-                    # print(type(text))
+                        # print(text)
 
->>>>>>> Stashed changes
 
 def drawGrid():
     for x in range(10):
@@ -653,12 +446,7 @@ def puzzle_gen():
     # convert into 2d array
     for i in range(0, 10):
         for j in range(0, 10):
-<<<<<<< Updated upstream
-            table[i][j] = result.get('panel').cells[i,j]
-    
-=======
             table[i][j] = result.get('panel').cells[i, j]
 
 
->>>>>>> Stashed changes
 main()
